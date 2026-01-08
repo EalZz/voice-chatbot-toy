b717c44c125b9c3e13de9d805cb3a4790c988f07
EalZz <clapa0211@gmail.com>
2026-01-08 16:54:34 +0900

    Update: 안드로이드 최신 반영 (Backup: backup_20260108_165349)

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
-from prometheus_fastapi_instrumentator import Instrumentator # 수정됨
+from prometheus_fastapi_instrumentator import Instrumentator
 from routers import router as chat_router
 
 app = FastAPI()
 
-# 프로메테우스 설정 (이 방식이 더 간단하고 강력합니다)
+# 프로메테우스 설정 
 Instrumentator().instrument(app).expose(app)
 
 # 라우터 등록
